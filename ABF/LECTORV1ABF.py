d109c1a34f3311374c9ae6581ab921d0035573e1
Cisco <cisco115_sisoyplic@hotmail.com>
2024-01-09 00:04:05 -0600

    Integracion con pandas

--- ABF/LECTORV1ABF.py
+++ ABF/LECTORV1ABF.py
@@ -1,7 +1,7 @@
 import pyabf
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
 # Ruta al archivo ABF
 ruta_archivo_abf = 'ABF/15n24009.abf'
 
@@ -24,11 +24,18 @@ if abf.sweepCount > 0:
     tiempo = np.transpose(abf.sweepX)
     np.transpose(tiempo)
     print('Dimensiones tiempo: ',tiempo.shape)
+    # Definimos una matriz para poder manipular los datos
+    abf.setSweep(sweepNumber=0,channel=2)
+    dataMatrix = np.array([abf.sweepY])
+    dataMatrix = np.transpose(dataMatrix)
+    # DataFrame con pandas
+    df = pd.DataFrame(dataMatrix, columns = ['Datos'])
+    df = df.drop(df.index[:4000])
+    print(f'Min Value: {df.min().min()}')
     # Graficamos cada canal para obtener la informacion
     fig, axs = plt.subplots(abf.channelCount,1,figsize=(10,6))
     for i in range(abf.channelCount):
         abf.setSweep(sweepNumber=0,channel=i)
-        np.array(abf.setSweepY)
         axs[i].plot(abf.sweepX, abf.sweepY, label=f'Canal {i+1}')
         axs[i].set_title(f"Datos del canales {i+1} durante el experimento")
         axs[i].set_xlabel("Tiempo (s)")
